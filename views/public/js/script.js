5b822e0f09702e10564eec212486e832574906b8
Brey012 <bhernandezos@cesde.net>
2024-11-05 16:17:07 -0500

    eventos en el doom

--- views/public/js/script.js
+++ views/public/js/script.js
@@ -1,12 +1,69 @@
-function Persona(nombre, edad, estatura, color) {
-  this.nombre = nombre;
-  this.edad = edad;
-  this.estatura = estatura;
-  this.color = color;
-  this.saludar = function() {
-    console.log(`Hola, me llamo ${this.nombre} y tengo ${this.edad}`)}
+// function Persona(nombre, edad, estatura, color) {
+//   this.nombre = nombre;
+//   this.edad = edad;
+//   this.estatura = estatura;
+//   this.color = color;
+//   this.saludar = function () {
+//     console.log(`Hola, me llamo ${this.nombre} y tengo ${this.edad}`);
+//   };
+// }
+
+// const Persona1 = new Persona("Breyner", 21, 1.77, "Canela pasion");
+
+// Persona1.saludar();
+
+// let Persona = {
+//   nombre: 'Breyner',
+//   edad: 21,
+//   estatura: 1.77,
+//   saludar: function saludar() {
+//     console.log('Hola mi nombre es ' + this.nombre)
+//   },
+//   direccion: {
+//     calle: 23,
+//     carrea:32,
+//     interior: 2
+//   }
+
+// }
+
+// Persona.saludar;
+
+let formularioLogin = document.getElementById("form-login");
+
+let btnFormRegistro = document.getElementById("btnFormRegistro");
+
+let btnFormLogin = document.getElementById("btnFormLogin");
+
+let inputs = document.getElementsByTagName("input");
+
+let registerCorreo = document.getElementById("register-correo");
+
+let regexEmail = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
+
+btnFormRegistro.addEventListener("click", () => {
+  document.getElementById("form-login").style.display = "flex";
+  document.getElementById("form-register").style.display = "none";
+});
+
+btnFormLogin.addEventListener("click", () => {
+  document.getElementById("form-login").style.display = "none";
+  document.getElementById("form-register").style.display = "flex";
+});
+
+for (let index = 0; index < inputs.length; index++) {
+  inputs[index].addEventListener("keyup", validarFormulario);
 }
 
-const Persona1 =  new Persona('Breyner', 21, 1.77, 'Canela pasion')
+function validarFormulario(e) {
+  switch (e.target.name) {
+    case "login-usuario":
+      console.log("Este es el input de login usuario");
+      break;
 
-Persona1.saludar();
+    default:
+      break;
+  }
+}
+
+registerCorreo.addEventListener("submit", () => {});
