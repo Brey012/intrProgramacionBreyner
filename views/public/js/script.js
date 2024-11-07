7b80704e0b1b1e00d1095cbfb6dddf211fd3c06b
Breyner012 <bhernandezos@cesde.net>
2024-11-07 15:51:47 -0500

    nada

--- views/public/js/script.js
+++ views/public/js/script.js
@@ -1,33 +1,4 @@
-// function Persona(nombre, edad, estatura, color) {
-//   this.nombre = nombre;
-//   this.edad = edad;
-//   this.estatura = estatura;
-//   this.color = color;
-//   this.saludar = function () {
-//     console.log(`Hola, me llamo ${this.nombre} y tengo ${this.edad}`);
-//   };
-// }
-
-// const Persona1 = new Persona("Breyner", 21, 1.77, "Canela pasion");
-
-// Persona1.saludar();
-
-// let Persona = {
-//   nombre: 'Breyner',
-//   edad: 21,
-//   estatura: 1.77,
-//   saludar: function saludar() {
-//     console.log('Hola mi nombre es ' + this.nombre)
-//   },
-//   direccion: {
-//     calle: 23,
-//     carrea:32,
-//     interior: 2
-//   }
-
-// }
-
-// Persona.saludar;
+import { expresionesRegulares } from "../../../utils/expresiones.js";
 
 let formularioLogin = document.getElementById("form-login");
 
@@ -36,10 +7,6 @@ let btnFormRegistro = document.getElementById("btnFormRegistro");
 let btnFormLogin = document.getElementById("btnFormLogin");
 
 let inputs = document.getElementsByTagName("input");
-
-let registerCorreo = document.getElementById("register-correo");
-
-let regexEmail = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
 
 btnFormRegistro.addEventListener("click", () => {
   document.getElementById("form-login").style.display = "flex";
@@ -57,13 +24,15 @@ for (let index = 0; index < inputs.length; index++) {
 
 function validarFormulario(e) {
   switch (e.target.name) {
-    case "login-usuario":
-      console.log("Este es el input de login usuario");
+    case "register-nombre":
+      if (expresionesRegulares.nombre.test(e.target.value)) {
+        console.log("Paso la validacion");
+      } else {
+        console.log("El campo solo permite letras en mayuscula");
+      }
       break;
 
     default:
       break;
   }
 }
-
-registerCorreo.addEventListener("submit", () => {});
